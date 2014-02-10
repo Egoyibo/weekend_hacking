406abcba3fcc90676738c05f73bb80bef20b5f72
Egoyibo <jessymong@gmail.com>
2014-02-10 00:13:48 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import Flask , render_template
 app = Flask (__name__)
 
 @app.route("/")
+@app.route("/index")
 def index():
 	return render_template("index.html")
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.route("/babaafrik")
+def static_tunde():
+	return render_template("tunde.html")
 
 
 if __name__ == "__main__":
